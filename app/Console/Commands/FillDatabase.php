53a9e716a1b8484e414f14e901e70c9af1d123f8
Jesus Rojas <jesus@invorious.com>
2024-04-26 02:50:05 -0500

    feat: getting data of randomuser :sparkles:

--- app/Console/Commands/FillDatabase.php
+++ app/Console/Commands/FillDatabase.php
@@ -13,24 +13,45 @@ class FillDatabase extends Command
   public function handle()
   {
     $news = $this->getNews();
-    logger($news);
+    $amountNews = count($news);
+    $users = $amountNews ? $this->getRandomUsers($amountNews) : [];
+    // foreach ($news as $newIndex => $new) {
+      
+    // }
+    logger($users);
   }
 
   public function getNews()
   {
-    $news = collect();
+    $news = [];
     try {
       $apiKey = env('API_NEWS_KEY');
       $data = Http::get(env('API_NEWS') . "?apiKey=$apiKey&country=us")->json();
-      $news = collect($data['articles'])->map(fn ($article) => ([
-        'publishedAt' => $article['publishedAt'],
-        'description' => $article['description'],
-        'title' => $article['title'],
-        'urlImage' => $article['urlToImage'],
-      ]));
+      $news = collect($data['articles'])
+        ->map(fn ($article) => ([
+          'publishedAt' => $article['publishedAt'],
+          'description' => $article['description'],
+          'title' => $article['title'],
+          'urlImage' => $article['urlToImage'],
+        ]))
+        ->values();
     } catch (\Throwable $th) {
       logger($th);
     }
     return $news;
+  }
+
+  public function getRandomUsers($amount = 5)
+  {
+    $users = [];
+    try {
+      $data = Http::get(env('API_RANDOM_USER') . "?results=$amount")->json();
+      $users = collect($data['results'])
+        ->map(fn ($user) => "{$user['name']['first']} {$user['name']['last']}")
+        ->values();
+    } catch (\Throwable $th) {
+      logger($th);
+    }
+    return $users;
   }
 }
